9209d2db11cda145fe1d1ef941a8f24ffaeb3898
riz4l <rizki.fahrizal@gmail.com>
2018-04-17 09:51:41 +0700

    add Requests/validasilogin.php add Requests/validasiregister.php add Requests/validasitambah.php

---
+++ privasi/app/Http/Requests/validasitambah.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class validasitambah extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nama'=>'required',
+            'alamat'=>'required',
+            'semester'=>'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nama.required'=>'Nama Harus diisi',
+            'alamat.required'=>'Alamat Harus diisi',
+            'semester.required'=>'Semester Harus diisi'
+        ];
+    }
+}
